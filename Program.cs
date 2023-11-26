ce7443a9b4f524198c46494a726daba832fe7e08
Boyarchuck81 <>
2023-11-26 18:07:47 +0300

    001

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+System.Console.WriteLine("Введите число: "); 
+int userNamber = Convert.ToInt32(Console.ReadLine());
+int result = userNamber * userNamber;
+Console.WriteLine(result);
